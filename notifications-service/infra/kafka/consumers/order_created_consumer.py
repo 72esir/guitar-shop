6fc708b14f2dada5f357d2c32ee774a60e8e9ccf
72esir <asalamatin640@gmail.com>
2026-05-09 14:26:10 +0500

    add buisness concumer to sending messages

---
+++ notifications-service/infra/kafka/consumers/order_created_consumer.py
@@ -0,0 +1,23 @@
+from broker.kafka.consumers.base_consumer import BaseConsumer
+from broker.kafka.client.kafka_client import KafkaClient
+
+class OrderCreatedConsumer(BaseConsumer):
+
+    topic = "order.created"
+    group_id = "order-service"
+
+    def __init__(
+        self, 
+        kafka_client: KafkaClient,
+        send_message_use_case, #TODO
+        ) -> None:
+        super().__init__(kafka_client)
+
+        self.send_message_use_case = send_message_use_case
+
+    async def process_message(self, message):
+
+        await self.send_message_use_case.execute(
+            
+        )
+
